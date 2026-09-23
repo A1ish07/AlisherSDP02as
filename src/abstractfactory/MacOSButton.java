f48778b1111259840f6691bf77b501cd9c3c3b3c
alisher <alimbayev.alisher07@gmail.com>
2026-09-23 12:08:26 +0500

    add abstractfactory

---
+++ src/abstractfactory/MacOSButton.java
@@ -0,0 +1,8 @@
+package abstractfactory;
+
+public class MacOSButton implements Button {
+    @Override
+    public void render() {
+        System.out.println("Rendering MacOS button");
+    }
+}
---
+++ src/abstractfactory/MacOSCheckbox.java
@@ -0,0 +1,8 @@
+package abstractfactory;
+
+public class MacOSCheckbox implements Checkbox {
+    @Override
+    public void render() {
+        System.out.println("Rendering MacOS checkbox");
+    }
+}
